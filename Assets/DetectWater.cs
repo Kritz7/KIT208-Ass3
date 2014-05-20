b11282a4bc57181fef2930fb3958af438278a1a4
Nick Blackburn <kritz117@gmail.com>
2014-05-20 15:04:47 +1000

    ass

---
+++ Assets/DetectWater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectWater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnParticleCollision(GameObject other)
+	{
+		if(other.name == "FireEffect")
+		{
+			//print ("Hit fire!");
+		}
+	}
+}
---
+++ Assets/FireHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireHealth : MonoBehaviour {
+
+	ParticleEmitter fireOut;
+	ParticleEmitter fireIn;
+	ParticleEmitter fireSmoke;
+
+	ParticleEmitter flameOut;
+	ParticleEmitter flameIn;
+	ParticleEmitter flameSmoke;
+
+    LinkedList<ParticleEmitter> particles = new LinkedList<ParticleEmitter>();
+
+	// Use this for initialization
+	void Start ()
+	{
+		fireOut = transform.FindChild ("OuterCore").GetComponent<ParticleEmitter> ();
+		fireIn = transform.FindChild ("InnerCore").GetComponent<ParticleEmitter> ();
+		fireSmoke = transform.FindChild ("smoke").GetComponent<ParticleEmitter> ();
+
+
+		flameOut = transform.FindChild ("Flame").FindChild ("OuterCore").GetComponent<ParticleEmitter>();
+		flameIn = transform.FindChild ("Flame").FindChild ("InnerCore").GetComponent<ParticleEmitter>();
+		flameSmoke = transform.FindChild ("Flame").FindChild ("Smoke").GetComponent<ParticleEmitter>();
+
+        particles.AddLast(fireOut);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
