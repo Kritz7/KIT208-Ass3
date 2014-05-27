b5ca8a8a13e1c22899592654342dd9b46453f03a
Antsiscool <antony.bowen@gmail.com>
2014-05-27 15:44:43 +1000

    PickUp now supports 3 items
    A third thing can be picked up. This has been referred to as "target". This is currently thought to be a person to be rescued, but may be changed to something.

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -1,22 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class PickUp : MonoBehaviour {
 	bool fireHosePickedUp = false;
 	bool axePickedUp = false;
+	bool targetPickedUp = false;
 	int equiped = 0;
-
+	
 	GameObject fireHose = null;
 	GameObject axe = null;
-
+	GameObject target = null;
+	
 	GameObject hands = null;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		hands = GameObject.Find("Main Camera/Hands");
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -29,28 +31,70 @@ public class PickUp : MonoBehaviour {
 			axe.transform.position = hands.transform.position;
 			axe.transform.rotation = hands.transform.rotation;
 		}
-
-		if (equiped == 1 && fireHosePickedUp) {
-			fireHose.SetActive(true);
+		if (target != null) {
+			target.transform.position = hands.transform.position;
+			target.transform.rotation = hands.transform.rotation;
+		}
+		
+		if (equiped == 1) {
+			if (fireHosePickedUp) {
+				fireHose.SetActive(true);
+				if (axePickedUp) {
+					axe.SetActive(false);
+				}
+				if (targetPickedUp) {
+					target.SetActive(false);
+				}
+			}
+			else {
+				equiped = 2;
+			}
+		}
+		if(equiped == 2) {
 			if (axePickedUp) {
-				axe.SetActive(false);
+				axe.SetActive(true);
+				if(fireHosePickedUp){
+					fireHose.SetActive(false);
+				}
+				if (targetPickedUp) {
+					target.SetActive(false);
+				}
+			}
+			else {
+				equiped = 3;
 			}
 		}
-		else if(equiped == 2 && axePickedUp) {
-			axe.SetActive(true);
-			if(fireHosePickedUp){
-				fireHose.SetActive(false);
+
+		if (equiped == 3) {
+			if (targetPickedUp) {
+				target.SetActive(true);
+				if (fireHosePickedUp){
+					fireHose.SetActive(false);
+				}
+				if (axePickedUp) {
+					axe.SetActive(false);
+				}
+			}
+			else {
+				equiped = 1;
 			}
 		}
+		if (equiped == 4) {
+			equiped = 1;
+		}
+
+		if (!fireHosePickedUp && !axePickedUp && !targetPickedUp) {
+			equiped = 0;
+		}
+	
 
 		if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-			if (fireHosePickedUp && axePickedUp) {
-				equiped = equiped % 2 + 1;
-				print ("Switching equiped item");
-			}
+			equiped += 1;
+			print ("Switching equiped item");
+
 		}
 	}
-
+	
 	void OnTriggerEnter(Collider other) {
 		string item = other.gameObject.name;
 		if (item == "Fire Hose") {
@@ -64,6 +108,12 @@ public class PickUp : MonoBehaviour {
 			axe = other.gameObject;
 			equiped = 2;
 			print ("Axe " + axe.name);
+		}
+		else if (item == "Target") {
+			targetPickedUp = true;
+			target = other.gameObject;
+			equiped = 3;
+			print ("Target " + target.name);
 		}
 	}
 }
